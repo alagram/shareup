dc6e49947cbd21d50cbd6742c9d5db1fc1d84d47
Albert Agram <albert.agram@gmail.com>
2014-07-18 13:11:24 +0000

    add set_csrf_cookie_for_ng to application_controller to combat cross-site scripting attacks in Angular

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_filter :set_csrf_cookie_for_ng
+
+  private
+
+  def set_csrf_cookie_for_ng
+    cookie['XSRF-TOKEN'] = from_authenticity_token if protect_from_forgery?
+  end
+
+  def verified_request?
+    super || from_authenticity_token == request.headers['HTTP_X_XSRF_TOKEN']
+  end
 end
