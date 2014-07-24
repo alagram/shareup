0dc42d3a839acd06f981d6317d7bb732c6f1b08a
Albert Agram <albert.agram@gmail.com>
2014-07-24 17:31:37 +0000

    Injected services SessionService, session, Share into HomeController

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -1,5 +1,6 @@
 angular.module('myApp.controllers', [])
-  .controller('HomeController', function($scope, ArticleService) {
+  .controller('HomeController', function($scope, ArticleService, SessionService, session, Share) {
+    $scope.user = session.user;
     ArticleService.getLatestFeed()
     .then(function(data) {
       $scope.articles = data;
